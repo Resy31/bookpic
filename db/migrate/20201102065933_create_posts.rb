f694cb1e37bacbae54295f041965a90b6e82dad1
Resy31 <resy3311yahoo.co.jp>
2020-11-17 02:24:41 +0900

    データベースにユーザーのIDを追加

--- db/migrate/20201102065933_create_posts.rb
+++ db/migrate/20201102065933_create_posts.rb
@@ -8,7 +8,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string  :story,           null: false
       # t.integer :category_id,     null: false
       # t.integer :recommend_id,    null: false
-      # t.integer :user_id,         null: false, foreign_key: true
+      t.integer :user_id,         null: false, foreign_key: true
       t.timestamps
     end
   end
