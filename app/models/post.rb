63ca69591ee8cf60daed03ac593ccac6d9643dbf
Resy31 <resy3311yahoo.co.jp>
2020-11-25 19:33:02 +0900

    モデルの追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_one_attached :image
 
   def self.search(search)
     if search != ""
